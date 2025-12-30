fa2ad7a94d5f4e2a685191a2c9cad679dbb037c9
Bernard Lambeau <bernard.lambeau@enspirit.be>
2025-12-30 09:52:54 +0100

    Back to elo-lang.org

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import { resolve } from 'path';
 
 export default defineConfig({
-  site: 'https://enspirit.github.io',
-  base: '/elo',
+  site: 'https://elo-lang.org',
+  base: '/',
   output: 'static',
   vite: {
     resolve: {
